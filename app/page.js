f843c3e0b778c4e8ad4f5b9041e0d1b0d3eadcf1
Adam Benjamin <adm.bjmn@gmail.com>
2025-04-25 13:38:35 +0100

    chore: add iu icons

--- app/page.js
+++ app/page.js
@@ -1,15 +1,3 @@
-import Image from "next/image";
-
 export default function LandingPage() {
-  return (
-    <div className="">
-      Hello World{" "}
-      <Image
-        src={"/icons/facebook.svg"}
-        alt={"test"}
-        height={200}
-        width={200}
-      />
-    </div>
-  );
+  return <div className="">Hello World</div>;
 }
