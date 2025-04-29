6a4d43b89dacbd5853df22e173855922c9b580c6
Adam Benjamin <adm.bjmn@gmail.com>
2025-04-29 10:30:11 +0100

    chore: move primary button comment to the top of file

--- components/atoms/PrimaryButton.tsx
+++ components/atoms/PrimaryButton.tsx
@@ -1,34 +1,4 @@
 "use client";
-import Link from "next/link";
-
-// Loading Spinner for pages waiting
-const Spinner = () => (
-  <svg
-    // width="26"
-    // height="26"
-    className="aspect-square size-full"
-    viewBox="0 0 24 24"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z"
-      opacity=".25"
-      fill="#01e0f4"
-    />
-    <path
-      fill="#01e0f4"
-      d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z"
-    >
-      <animateTransform
-        attributeName="transform"
-        type="rotate"
-        dur="0.75s"
-        values="0 12 12;360 12 12"
-        repeatCount="indefinite"
-      />
-    </path>
-  </svg>
-);
 
 /**
  * Reusable Button component that can be used as either a clickable button or a link.
@@ -59,6 +29,37 @@ const Spinner = () => (
  * - The link behavior can open in a new tab if `openInNew` is set to true.
  * - The `setBack` prop can be used show a different styling to set the button back from other buttons - ie: in a tabs array.
  */
+
+import Link from "next/link";
+
+// Loading Spinner for pages waiting
+const Spinner = () => (
+  <svg
+    // width="26"
+    // height="26"
+    className="aspect-square size-full"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z"
+      opacity=".25"
+      fill="#01e0f4"
+    />
+    <path
+      fill="#01e0f4"
+      d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z"
+    >
+      <animateTransform
+        attributeName="transform"
+        type="rotate"
+        dur="0.75s"
+        values="0 12 12;360 12 12"
+        repeatCount="indefinite"
+      />
+    </path>
+  </svg>
+);
 
 // typescript annotations
 interface ButtonProps {
