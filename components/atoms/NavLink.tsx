7d56899d868821b86bd1741435c895bccab4ec21
Adam Benjamin <adm.bjmn@gmail.com>
2025-04-29 08:56:39 +0100

    chore: add accessibity tags to Navlink

--- components/atoms/NavLink.tsx
+++ components/atoms/NavLink.tsx
@@ -4,6 +4,8 @@ export default function NavLink({ link, label }) {
   return (
     <Link
       href={link}
+      id={label}
+      aria-label={`Link to ${label}`}
       className="from-forge-gradient-from to-forge-gradient-to flex w-fit items-center justify-center bg-gradient-to-r bg-clip-text font-light text-white transition-all duration-75 hover:scale-105 hover:font-semibold hover:text-transparent active:scale-95"
     >
       {label}
