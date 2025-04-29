34ae03a27f59c374dc3e87f19dcaeeac492455bf
Adam Benjamin <adm.bjmn@gmail.com>
2025-04-29 09:43:17 +0100

    feat: add social links component

---
+++ components/atoms/SocialsLink.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function SocialsLink({ socialLink }) {
+  const { key, link, icon } = socialLink;
+
+  if (!link) {
+    return null;
+  }
+  return (
+    <Link
+      href={link}
+      id={key}
+      aria-label={`Link to ${key}`}
+      className="relative flex aspect-square size-6 items-center justify-center transition duration-75 hover:scale-105 active:scale-95"
+      rel="noopener noreferrer"
+      target="_blank"
+    >
+      <Image
+        src={icon.sourceURL}
+        alt={icon.altText}
+        fill
+        className="object-contain"
+      />
+    </Link>
+  );
+}
--- components/molecules/EndCap.tsx
+++ components/molecules/EndCap.tsx
@@ -8,10 +8,10 @@
  *    - A `socials` array where each item includes a `key`, `link`, and `icon`.
  * - Layout the elements for both desktop and mobile based on the Figma design.
  * - Use Array.map() to dynamically render each social icon as a link using the "../atoms/SocialsLink.tsx" component.
- * - Ensure that social links open in a new tab (`target="_blank"` and `rel="noopener noreferrer"`).
+ *
  *
  * - Add a thin scroll progress bar at the very bottom of the page.
- * - Create the Progress bar as a separate atom inside "../atoms/ProgressBar.tsx"
+ * - Create the Progress bar as a separate atom inside "../atoms/ProgressBar.tsx" - more info can be found inside this component
  *
  * - Use Tailwind CSS for layout, spacing, colors, and responsiveness.
  */
@@ -19,7 +19,7 @@
 import endCapData from "@/data/endCap";
 
 export default function EndCap() {
-  // console.log(endCapData);
+  // console.log(endCapData.socials[0]);
 
   return <div>End Cap</div>;
 }
